fbb47ca626bcf58ab7214c9614cd4113221fe51a
sam4215 <sam@example.com>
2016-07-19 20:32:40 +0100

    Begin development
    Beginning development on a new branch

---
+++ src/main/java/me/sam4215/accountable/Accountable.java
@@ -0,0 +1,20 @@
+package me.sam4215.accountable;
+
+/**
+ * Accountable.java is the main class in Accountable
+ *
+ * Accountable is a tool you can count on.
+ */
+
+/**
+ * This program's license can be found at LICENSE.md.
+ * <p>
+ * If LICENSE.md does not exist throughout the entire project,
+ * you may assume that this code is under the MIT license.
+ * <p>
+ * Created by sam4215 (possibly for another organization)
+ * on 7/7/2016 in ${PACKAGE}
+ */
+public class Accountable {
+    //
+}
